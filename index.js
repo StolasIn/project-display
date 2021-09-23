3c85d783e38cbecb11d287b7d7f680aecf7ada5b
StolasIn <whale999999999@gmail.com>
2021-09-23 14:00:24 +0800

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-function mul(n1,n2){
-	let res=n1*n2;
-	return res;
+var str = "code"
+
+function print(){
+	let n = document.getElementById('see').value;
+	alert('welcome ' + n);
+	document.getElementById('love').innerText='ina ina ina~~~';
+}
+
+function insert_tako(){
+	var img = document.createElement("img");
+	img.src="pic/ina/long tako2.jpg";
+	img.setAttribute("width","400px");
+	var myDiv = document.getElementById('long_tako_insert');
+	myDiv.appendChild(img);
+}
+
+function reset_tako(){
+	document.getElementById('long_tako_insert').innerHTML="";
+}
+
+function sett(){
+	var code = document.createElement("code");
+	var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "code/spilt.txt", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+	/*code.innerHTML=s;
+	document.getElementById('hi').appendChild(code);*/
 }
--- python/bs4/opgg.py
+++
@@ -1,5 +0,0 @@
-import requests as req
-from bs4 import BeautifulSoup
-
-res = req.get('https://tw.op.gg/')
-print(res.text)
